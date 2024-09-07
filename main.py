4b036398bc68599584967d8b5d4ce780ac28e97a
Ryan D. Najac <ryan.najac@columbia.edu>
2024-09-07 18:27:29 -0400

    update docstrings

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from datetime import datetime
 
 
 def get_file_date(page):
-    """
-    Returns the last modification date of the given page's source file.
-    """
+    """Returns the last modification date of the given page's source file."""
     if page and hasattr(page, "file") and hasattr(page.file, "abs_src_path"):
         stat = os.stat(page.file.abs_src_path)
         return datetime.fromtimestamp(stat.st_mtime).strftime("%Y-%m-%d")
@@ -13,22 +11,14 @@ def get_file_date(page):
 
 
 def define_env(env):
-    """
-    This is the hook for defining variables, macros and filters
-    """
+    """This is the hook for defining variables, macros and filtersa."""
 
     @env.macro
     def get_file_date_macro(page):
-        """
-        Macro to call get_file_date in the templating environment.
-        """
+        """Macro to call get_file_date in the templating environment."""
         return get_file_date(page)
 
 
 def on_post_page_macros(env):
-    """
-    Actions to be done after macro interpretation,
-    when the markdown is already generated
-    """
-    # Use the macro function
+    """Actions after macro interpretation and markdown generation."""
     env.page.meta["file_date"] = get_file_date(env.page)
